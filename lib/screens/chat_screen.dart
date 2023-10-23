2e21b2e9a2f655697496fe3c51d9bddc9960ca27
somelelequse <QSXSOM001@myuct.ac.za>
2023-10-24 00:19:01 +0200

    updated registration/chat_screen with firebase authentication.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flash_chat_v2/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -11,6 +12,27 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _auth = FirebaseAuth.instance;
+  late User loggedInUser;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  Future<void> getCurrentUser() async {
+    try {
+      final user = _auth.currentUser;
+      if (user != null) {
+        loggedInUser = user;
+        print(loggedInUser.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flash_chat_v2/components/rounded_button.dart';
 import 'package:flash_chat_v2/constants.dart';
+import 'package:flash_chat_v2/screens/chat_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({super.key});
@@ -12,11 +14,21 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
+  bool isUserNull = true;
   late String email;
   late String password;
 
   @override
   Widget build(BuildContext context) {
+    void userNullChecker() {
+      if (isUserNull == false) {
+        Navigator.pushNamed(context, ChatScreen.id);
+      } else {
+        print('user not found.');
+      }
+    }
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
@@ -62,9 +74,20 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             RoundedButton(
               text: 'Register',
               color: Colors.blueAccent,
-              action: () {
-                print('your email is $email');
-                print('your password is $password');
+              action: () async {
+                try {
+                  final newUser = await _auth.createUserWithEmailAndPassword(
+                    email: email,
+                    password: password,
+                  );
+
+                  if (newUser != null) {
+                    isUserNull = false;
+                    userNullChecker();
+                  }
+                } catch (e) {
+                  print(e);
+                }
               },
             )
           ],
